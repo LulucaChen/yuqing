5c1e380c347aeb3edf1c96bbade04e2c80a1d10a
LulucaChen <793492054@qq.com>
2019-11-20 17:19:43 +0800

    //

---
+++ yuqing/src/components/Lefttab.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="headtab">
+    <el-menu class="el-menu-demo" mode="vertical" >
+      <el-menu-item v-for="idx in headtitles" :key="idx" >{{idx}}</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<style scoped>
+</style>
+<script>
+import 'element-ui/lib/theme-chalk/index.css'
+
+export default {
+  name: "Tab",
+  data() {
+    return {
+      headtitles: [
+        "首页",
+        "舆情分析",
+        "预警通报",
+        "数据报告",
+        "系统设置",
+        "下载中心"
+      ]
+    };
+  }
+};
+</script>
